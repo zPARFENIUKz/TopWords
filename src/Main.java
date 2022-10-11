ac55b344300d41d9c2032ef0f96ae67a88816b94
Gregory <parfeniuk33@gmail.com>
2022-10-11 21:22:23 +0300

    smthng

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Main {
+import java.util.List;
+
+
+public class Main {
+    public static void main(String[] args){
+        /*
+        List<String> topWords = TopWords.topWords(Stream
+                .of("In a village of La Mancha, the name of which I have no desire to call to",
+                        "mind, there lived not long since one of those gentlemen that keep a lance",
+                        "in the lance-rack, an old buckler, a lean hack, and a greyhound for",
+                        "coursing. An olla of rather more beef than mutton, a salad on most",
+                        "nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra",
+                        "on Sundays, made away with three-quarters of his income.")
+                .collect(Collectors.joining("\n")));
+         */
+        List<String> topWords = TopWords.topWords("  //wont won't won't ");
+        for (final String word : topWords){
+            System.out.print(word + " ");
+        }
+        System.out.println();
+        System.out.println(topWords.getClass());
+    }
+
 }
--- src/TopWords.java
+++ src/TopWords.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class TopWords {
+import java.util.*;
+
+public class TopWords {
+    public static List<String> topWords(final String str) throws NullPointerException{
+        if (str == null) throw new NullPointerException("str cannot be null");
+        final List<String> words = Arrays.stream(str.replaceAll("[^a-zA-z']", " ").replaceAll(" {2}", " ").strip().toLowerCase().split(" ")).toList();
+        final Set<String> uniqueWords = new HashSet<>(words);
+        final List<String> topWords = uniqueWords.stream().sorted(Comparator.comparing(word -> -Collections.frequency(words, word))).toList();
+        return topWords.subList(0, Math.min(3, topWords.size()));
+    }
 }
+
